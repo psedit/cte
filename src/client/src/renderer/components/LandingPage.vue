1d95a3ecc220ea1c25f6d6ec1ba6d780cc6922f1
= <=>
2019-06-06 00:21:01 +0200

    Working codemirror editor component with demo code

--- src/client/src/renderer/components/LandingPage.vue
+++ src/client/src/renderer/components/LandingPage.vue
@@ -1,40 +1,16 @@
 <template>
   <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
-    <main>
-      <div class="left-side">
-        <span class="title">
-          Welcome to your new project!
-        </span>
-        <system-information></system-information>
-      </div>
-
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-        </div>
-      </div>
-    </main>
+    <editor/>
   </div>
 </template>
 
 <script>
-  import SystemInformation from './LandingPage/SystemInformation'
+  // import SystemInformation from './LandingPage/SystemInformation'
+  import Editor from './Editor'
 
   export default {
     name: 'landing-page',
-    components: { SystemInformation },
+    components: { Editor },
     methods: {
       open (link) {
         this.$electron.shell.openExternal(link)
@@ -62,7 +38,6 @@
         rgba(229, 229, 229, .9) 100%
       );
     height: 100vh;
-    padding: 60px 80px;
     width: 100vw;
   }
 
