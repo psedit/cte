790f890cd6d33cb951263965e7d668c8be4097d4
Sam van Kampen <sam@tehsvk.net>
2019-06-06 13:52:14 +0200

    Let AccountManager inherit from Service and register its message types.

--- src/server/service/account_manager.py
+++ src/server/service/account_manager.py
@@ -1,20 +1,25 @@
+from service import Service, message_type
 from client import Address
 import Pyro4
 from typing import Dict, List, Tuple
 
 
 @Pyro4.expose
-class AccountManager():
+class AccountManager(Service):
     """
     Service responsible for handling all account related messages.
 
     Handles: login, logout, account registration, account deletion,
     account management (nomen est omen) and permission-related requests.
     """
-    def __init__(self, msg_pass):
+    _wanted_msg_types = [
+            "account-login"
+            ]
+
+    def __init__(self, msg_bus):
+        super().__init__(msg_bus)
         self._msg_pass = msg_pass
 
-    def login(self, addr: Address, uname: str, passwd: str):
+    @message_type("account-login")
+    def login(self, msg):
         pass
-
-
