6e29d1ab735753fb94a4f7254004b6d4e1cd6bc3
C9 <robinwacco11@hotmail.com>
2019-06-07 13:43:17 +0200

    Make serving files possible.

--- src/server/service/filesystem.py
+++ src/server/service/filesystem.py
@@ -1,4 +1,4 @@
-from fileio import ServerFile
+from server_file import ServerFile
 from typing import Dict, List
 from service import Service, message_type
 import sys
@@ -8,39 +8,43 @@ import Pyro4
 
 @Pyro4.expose
 @Pyro4.behavior(instance_mode="single")
-class FileSystem(Service):
-    # Files sorted by path relative to root dir
-    file_dict: Dict[str,ServerFile] = {}
-    root_dir: str
-
-    _wanted_msg_types = [
-            "request_file"
-        ]
+class Filesystem(Service):
+    """
+    
+    """
 
     def __init__(self, msg_bus) -> None:
         super().__init__(msg_bus)
 
+        # Files sorted by path relative to root dir        
+        self.file_dict: Dict[str, ServerFile] = {}
+
         # Check server config for root directory
         # TODO: retrieve from server
-        self.root_dir = os.getcwd().replace('service','') + "test" + os.sep
-
-    def add_file(self, path: str) -> None:
-        """Add the file to the FileSystem. Path file is relative to root
+        self.root_dir: str = os.path.realpath('../test/')
+    
+    @message_type("file-add")
+    def add_file(self, msg) -> None:
+        """
+        Add the file to the Filesystem. Path file is relative to root
         directory.
         """
-
+        
+        path = msg['content']['file_path']
         self.file_dict[path] = ServerFile(self.root_dir, path)
 
 
     def list_files(self) -> List[str]:
-        """Lists all files currently within the file system, relative to
+        """
+        Lists all files currently within the file system, relative to
         the root directory.
         """
 
         return list(self.file_dict.keys())
 
     def list_files_available(self) -> List[str]:
-        """Lists all files currently available, but not necessarily added,
+        """
+        Lists all files currently available, but not necessarily added,
         to the file system, relative to the root directory.
         """
 
@@ -49,16 +53,17 @@ class FileSystem(Service):
 
         for root, dirs, files in os.walk(root_dir):
             for f in files:
-                file_list.append((root + os.sep + f).replace(root_dir,""))
+                file_list.append(os.path.join(root, f).replace(root_dir,""))
             for d in dirs:
-                file_list.append((root + os.sep + d + os.sep).replace(root_dir,""))
+                file_list.append(os.path.join(root, d).replace(root_dir,""))
 
         return file_list
 
-    @message_type("file_content_request")
+    @message_type("file-content-request")
     def _process_file_content_request(self, msg) -> None:
-        """Take the file content request message and construct the appropriate response,
-        sending the block via a new 'file_content_response' message.
+        """
+        Take the file content request message and construct the appropriate response,
+        sending the block via a new 'file-content-response' message.
         """
         content = msg["content"]
 
@@ -69,15 +74,13 @@ class FileSystem(Service):
 
         block = self.get_block(file_path, start, end)
 
-        response_content = {
-                    "request_address": address,
-                    "file_content": block,
-                }
-        self._send_message("file_content_response", response_content)
+        response_content = {"file_content": block}
+        self._send_message_client("file-content-response", response_content, address)
 
 
     def get_block(self, path: str, start: int = 0, end: int = -1) -> List[str]:
-        """Returns all line between (and including) a start and end line, split
+        """
+        Returns all line between (and including) a start and end line, split
         up per line, of the specified file within the file system.
 
         Keyword arguments:
@@ -91,7 +94,17 @@ class FileSystem(Service):
         else:
             print("Error: File is not present in the file system.")
             return None
+            
+            
+    @message_type("cursor-move")
+    def _move_cursor(self, msg) -> None:
+        pass
+        # response_content = {
+                
+        #     }
+        
+        # self._send_message_client("cursor-move-broadcast", response_content)
 
 
 if __name__ == "__main__":
-    FileSystem.start()
+    Filesystem.start()
--- src/server/service/server_file.py
+++ src/server/service/server_file.py
@@ -1,28 +1,25 @@
 import mmap
 from typing import List, Tuple
-# from client import Address
+from client import Address
 import os
 
+Range = Tuple[int, int]
 
 class ServerFile:
-    root_dir: str
-    file_path_relative: str
-    file_mmap: mmap
-    # A list of locks: (Client Address, start line, end line)
-    # TODO: after merge: use 'Address' class
-    lock_list: List[Tuple[Tuple[str,int],int,int]] = []
-
     def __init__(self, root: str, path: str) -> None:
-        self.root_dir = root
-        self.file_path_relative = path
+        # A list of locks: (Client Address, start line, end line)
+        self.lock_list: Dict[Address, Range] = {}
+        self.cursor_list: Dict[Adress, Tuple[int,int]] = {}
+        self.root_dir: str = root
+        self.file_path_relative: str = path
+        self.file_mmap: mmap
 
         self.load_file()
 
     def load_file(self) -> None:
         """Create the mmap file object (Loads the file into virtual memory).
         """
-
-        f = os.open(self.root_dir + self.file_path_relative, os.O_RDWR)
+        f = os.open(os.path.join(self.root_dir, self.file_path_relative), os.O_RDWR)
         self.file_mmap = mmap.mmap(f,0,prot=mmap.PROT_READ | mmap.PROT_WRITE)
 
     def retrieve_block(self, start: int = 0, end: int = -1) -> List[str]:
@@ -33,14 +30,13 @@ class ServerFile:
         start -- Line number indicating start position
         end -- Line number indicating end position, -1 indicates the last line.
         """
-
-        if self.file_mmap is not None:
+        if self.file_mmap:
             self.file_mmap.seek(0)
             block: List[str] = []
 
             for i, line in enumerate(iter(self.file_mmap.readline, b"")):
                 if start <= i and (end < 0 or i <= end):
-                    block.append(line)
+                    block.append(line.decode('utf-8'))
 
             return block
         else:
@@ -50,13 +46,11 @@ class ServerFile:
         """Writes the current buffer to the file on disk, although the mmap
         automatically writes to disk in most instances.
         """
-
         self.file_mmap.flush()
 
     def update_buffer(self, delta) -> None:
         """ Writes the the delta contents (= file change) to the buffer.
         """
-
         # TODO: Keep locks in mind
         # TODO: Return error code(?)
         pass
@@ -68,3 +62,13 @@ class ServerFile:
     def remove_lock(self, delta) -> None:
         # TODO: Return error code(?)
         pass
+    
+    def move_cursor(self, client: Address, row: int, column: int) -> None:
+        self.cursor_list[client] = (row, column)
+    
+    def get_cursor(self, client) -> Tuple[int,int]:
+        return self.cursor_list[client]
+        
+    def drop_client(self, client) -> None:
+        self.cursor_list,pop(client)
+
