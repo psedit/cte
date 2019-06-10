0ee794ecba9146e900b8c02f1b7886480656258e
C9 <robinwacco11@hotmail.com>
2019-06-10 12:18:40 +0200

    Server side dir listing & file join/leave

--- src/server/services/filesystem.py
+++ src/server/services/filesystem.py
@@ -13,90 +13,158 @@ class Filesystem(Service):
     """
     def __init__(self, msg_bus) -> None:
         super().__init__(msg_bus)
-
-        # Files sorted by path relative to root dir
-        self.file_dict: Dict[str, ServerFile] = {}
-
         # Check server config for root directory
         # TODO: retrieve from server
         self.root_dir: str = os.path.realpath('../test/')
 
-    @message_type("file-add")
-    def add_file(self, msg) -> None:
+        # Files sorted by path relative to root dir
+        self.file_dict: Dict[str, ServerFile] = {}
+        self.root_tree = self.parse_walk(list(os.walk(self.root_dir)), self.root_dir)
+
+    def add_file(self, file_path: str) -> None:
         """
         Add the file to the Filesystem. Path file is relative to root
         directory.
         """
-        path = msg['content']['file_path']
-        self.file_dict[path] = ServerFile(self.root_dir, path)
+        
+        if file_path not in self.file_dict.keys():
+            self.file_dict[file_path] = ServerFile(self.root_dir, file_path)
 
     def list_files(self) -> List[str]:
         """
-        Lists all files currently within the file system, relative to
+        Lists all files currently within the file system (in RAM), relative to
         the root directory.
         """
         return list(self.file_dict.keys())
 
-    def list_files_available(self) -> List[str]:
+    def parse_walk(self, walk, path):
         """
-        Lists all files currently available, but not necessarily added,
-        to the file system, relative to the root directory.
+        Creates directory tree of the root directory.
         """
-        root_dir = self.root_dir()[:-1]
-        file_list = []
+        dir_tup = [t for t in walk if t[0] == path][0]
+        walk.remove(dir_tup)
+        tree = []
+        
+        for dir_name in dir_tup[1]:
+            tree.append((dir_name, self.parse_walk(walk, os.path.join(path, dir_name))))
+        for file_name in dir_tup[2]:
+            tree.append(file_name)
+            
+        return tree
 
-        for root, dirs, files in os.walk(root_dir):
-            for f in files:
-                file_list.append(os.path.join(root, f).replace(root_dir, ""))
-            for d in dirs:
-                file_list.append(os.path.join(root, d).replace(root_dir, ""))
+    def get_block(self, path: str, start: int = 0, length: int = -1) -> List[str]:
+        """
+        Returns all 'length' lines starting from 'start', of the specified file 
+        within the file system.
 
-        return file_list
+        Keyword arguments:
+        path -- Path to the file relative to the root directory.
+        start -- Line number indicating start position
+        length -- Amount of lines to return, -1 indicates until the last line.
+        """
+        if path in self.file_dict.keys():
+            return self.file_dict[path].retrieve_block(start, length)
+        else:
+            raise ValueError("File is not present in file system RAM.")
 
     @message_type("file-content-request")
     def _process_file_content_request(self, msg) -> None:
         """
-        Take the file content request message and construct the appropriate response,
-        sending the block via a new 'file-content-response' message.
+        Take the file content request message and construct the appropriate 
+        response, sending the block via a new 'file-content-response' message.
         """
         content = msg["content"]
 
         end = content["end"]
         start = content["start"]
         file_path = content["file"]
-        address = content["request_address"]
+        address = content["address"]
 
-        block = self.get_block(file_path, start, end)
-
-        response_content = {"file_content": block}
-        self._send_message_client(
-            "file-content-response", response_content, address)
-
-    def get_block(self, path: str, start: int = 0, end: int = -1) -> List[str]:
-        """
-        Returns all line between (and including) a start and end line, split
-        up per line, of the specified file within the file system.
-
-        Keyword arguments:
-        path -- Path to the file relative to the root directory.
-        start -- Line number indicating start position
-        end -- Line number indicating end position, -1 indicates the last line.
-        """
-        if path in self.file_dict.keys():
-            return self.file_dict[path].retrieve_block(start, end)
+        if file_path not in self.file_dict.keys():
+            # TODO: send exception to client
+            # "File is not in system RAM. Join the file to load it to memory."
+            pass
+        if not self.file_dict[file_path].is_joined(address):
+            # TODO: send exception to client
+            # "Please join the file before requesting its contents."
+            pass 
         else:
-            print("Error: File is not present in the file system.")
-            return None
+            block = self.get_block(file_path, start, end)
+    
+            response_content = {"file_content": block, "address": address}
+            self._send_message_client("file-content-response", response_content, 
+                                      address)
 
+   
+    @message_type("file-list-request")
+    def _send_file_list(self, msg) -> None:
+        content = msg["content"]
+        address = msg["sender"][0]
+        
+        net_msg = {
+            "root_tree": self.root_tree
+        }
+        
+        self._send_message_client("file-list-response", net_msg, address)
+            
     @message_type("cursor-move")
     def _move_cursor(self, msg) -> None:
-        pass
+        
         # response_content = {
 
         #     }
 
         # self._send_message_client("cursor-move-broadcast", response_content)
+        
+        pass
+    
+    @message_type("file-join")
+    def _file_add_client(self, msg) -> None:
+        """
+        Add the client from the file specified in the message.
+        Add the file to RAM if necessary.
+        """
+        content = msg["content"]
 
+        file = content["file_path"]
+        address = content["address"]
+        
+        if not os.path.isfile(os.path.join(self.root_dir, file)):
+            # TODO: send exception to client
+            # "This file is not present on the server."
+            return
+        
+        # Add the file to RAM if necessary.
+        if file not in self.file_dict.keys():
+            self.file_dict[file] = ServerFile(self.root_dir, file)
+            
+        # Add the file to the client list in the ServerFile class.
+        self.file_dict[file].move_cursor(address, 0, 0)
+
+    
+    @message_type("file-leave")
+    def _file_remove_client(self, msg) -> None:
+        """
+        Remove the client from the file specified in the message.
+        Remove the file from RAM if no clients are connected within the file.
+        """
+        content = msg["content"]
+
+        file = content["file_path"]
+        address = content["address"]
+        force = content["force_exit"]
+
+        if (force == False and self.file_dict[file].client_count() == 1 and 
+                self.file_dict[file].saved_status() is False):
+            # TODO: send exception to client
+            # "First save the file or resend request with force_exit = 1"
+            return
+
+        self.file_dict[file].drop_client(address)
+
+        # Remove the file from RAM if necessary.
+        if self.file_dict[file].client_count() == 0:
+            self.file_dict.pop(file)
 
 if __name__ == "__main__":
     Filesystem.start()
