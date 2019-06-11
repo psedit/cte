d3fc31c001d9263888d0c7ce2edf99d2dbef82af
Mund Vetter <mundvetter@gmail.com>
2019-06-11 11:59:58 +0200

    added remove and add tab

--- src/client/src/renderer/store/modules/fileTracker.js
+++ src/client/src/renderer/store/modules/fileTracker.js
@@ -11,6 +11,20 @@ const mutations = {
     state.code = newCode
     console.log('State is: ' + state.code)
   },
+  /** Adds a tab to state
+   * @param {Object} state
+   * @param {string} newTab
+   */
+  addTab (state, newTab) {
+    state.push(newTab)
+  },
+  /** Removes a tab from state
+   * @param {Object} state
+   * @param {string} tabToRemove
+   */
+  removeTab (state, tabToRemove) {
+    state.tabs.filter(x => x !== tabToRemove)
+  },
   /** Updates the filepaths
    * @param {Object} state
    * @param {Object[]} filePaths
@@ -22,12 +36,13 @@ const mutations = {
 
 const actions = {
   /** Opens a file from the root of the project, and updates the code.
-   * Also changes the openedFile state.
+   * Also changes the openedFile state. Add the file to the tabs.
    * @param {Object} state
    * @param {string} filePath
    */
   openFile (state, filePath) {
     state.openedFile = filePath
+
     fs.readFile(filePath.substring(0, filePath.length - 1), 'utf8', (err, data) => {
       if (err) {
         console.error(err)
