c85d58e26fb33097ef13e03e2e7b96e56d5ba44c
Maxim van den Berg <maximvdberg@gmail.com>
2019-06-11 11:47:45 +0200

    added changes from #72

--- src/client/src/renderer/components/Sidebar.vue
+++ src/client/src/renderer/components/Sidebar.vue
@@ -22,7 +22,6 @@
   import BackIcon from 'vue-material-design-icons/ArrowLeft'
   import FolderIcon from 'vue-material-design-icons/Folder'
   import FileIcon from 'vue-material-design-icons/File'
-
   export default {
     name: 'sidebar',
     data () {
@@ -36,33 +35,17 @@
       FolderIcon,
       FileIcon
     },
+    // FIXME: use the data from vuex
     computed: {
       files () {
         /* Create list of all files in current folder. */
         const fs = require('fs')
         const currFolder = this.currFolder
-
         // let files = [{name: '\ud83d\udd19', type: 'dir', path: parentFolder},
         //   {name: 'HOME', type: 'dir', path: `./`}]
         let files = []
 
-        /* Loop over all files in current directory and add
-         * object to files array, storing the name and type
-         * (either directory or file) of the file.
-         * For sorting purposes, first push all directories
-         * and then all other files. */
-        fs.readdirSync(currFolder).forEach(file => {
-          if (fs.lstatSync(currFolder + file).isDirectory()) {
-            files.push({name: file, type: 'dir', path: `${currFolder}${file}/`})
-          }
-        })
-
-        fs.readdirSync(currFolder).forEach(file => {
-          if (!fs.lstatSync(currFolder + file).isDirectory()) {
-            files.push({name: file, type: 'file', path: `${currFolder}${file}/`})
-          }
-        })
-
+        this.$store.commit('updateFiles', files)
         return files
       }
     },
@@ -85,7 +68,6 @@
           path: parentFolder
         })
       },
-
       home () {
         this.fileClick({
           // name: parentFolder,
@@ -93,27 +75,11 @@
           path: './'
         })
       },
-
       fileClick (file) {
         if (file.type === 'dir') {
           this.currFolder = file.path
         } else {
-          const store = this.$store
-          const fs = require('fs')
-
-          console.log('test1!!!!!')
-          fs.readFile(file.path.substring(0, file.path.length - 1), 'utf8', (err, data) => {
-            console.log('EINDELIJK')
-            if (err) {
-              throw err
-            }
-            console.log('NIEUWE CODE LALALA: ' + data)
-            store.dispatch('updateCodeAction', data)
-          })
-          console.log('test2!!!!!')
-          // const file = ev.target.files[0]
-          // const reader = new FileReader()
-          // reader.readAsText(file)
+          this.$store.dispatch('openFile', file.path)
         }
       }
     }
@@ -122,19 +88,16 @@
 
 <style scoped lang="scss">
   $padding: 1em;
-
   .sidenav {
     background-color: #111;
     display: grid;
     grid-template-rows: auto 1fr;
     height: 100vh;
   }
-
   .curr-folder {
     color: #ccc;
     font-size: 0.8em;
   }
-
   #toolbar {
     background-color: #333;
     color: #fff;
@@ -143,51 +106,42 @@
     display: grid;
     grid-template-columns: 1fr auto auto;
     grid-gap: 0.5em;
+    height: 50px;
   }
-
   #file-list {
     overflow-y: auto;
     padding: 0 1em;
     margin-top: .5em;
     list-style-type: none;
   }
-
   .button {
     cursor: pointer;
     :hover {
       color: #fff;
     }
   }
-
   .dir {
     color: #ccc;
     cursor:pointer;
-
     &:hover {
       color: rgb(102, 15, 102);
     }
   }
-
   .file {
     color: #fff;
-
     &:hover {
       color: rgb(102, 15, 102);
       cursor: pointer;
     }
   }
-
-
   .sidenav a {
     /*padding: 6px 8px 6px 16px;*/
     text-decoration: none; /* No underline in links. */
     font-size: 25px;
     color: #818181;
     display: block;
-
     &:hover {
       color: #f1f1f1;
-
     }
   }
 </style>
--- src/client/src/renderer/store/modules/fileTracker.js
+++ src/client/src/renderer/store/modules/fileTracker.js
@@ -1,15 +1,41 @@
+const fs = require('fs')
 const state = {
-  code: ''
+  code: '',
+  openedFile: '',
+  filePaths: '',
+  tabs: ['']
 }
 
 const mutations = {
   updateCode (state, newCode) {
     state.code = newCode
     console.log('State is: ' + state.code)
+  },
+  /** Updates the filepaths
+   * @param {Object} state
+   * @param {Object[]} filePaths
+   */
+  updateFiles (state, filePaths) {
+    state.filePaths = filePaths
   }
 }
 
 const actions = {
+  /** Opens a file from the root of the project, and updates the code.
+   * Also changes the openedFile state.
+   * @param {Object} state
+   * @param {string} filePath
+   */
+  openFile (state, filePath) {
+    state.openedFile = filePath
+    fs.readFile(filePath.substring(0, filePath.length - 1), 'utf8', (err, data) => {
+      if (err) {
+        console.error(err)
+        state.commit('updateCode', `Something went wrong: ${err}`)
+      }
+      state.commit('updateCode', data)
+    })
+  },
   updateCodeAction (state, newCode) {
     state.commit('updateCode', newCode)
   }
@@ -20,3 +46,21 @@ export default {
   mutations,
   actions
 }
+  // readLocalDirTree (root) {
+  //   /* Loop over all files in current directory and add
+  //    * object to files array, storing the name and type
+  //    * (either directory or file) of the file.
+  //    * For sorting purposes, first push all directories
+  //    * and then all other files. */
+  //   fs.readdirSync(currFolder).forEach(file => {
+  //     if (fs.lstatSync(currFolder + file).isDirectory()) {
+  //       files.push({name: file, type: 'dir', path: `${currFolder}${file}/`})
+  //     }
+  //   })
+  //
+  //   fs.readdirSync(currFolder).forEach(file => {
+  //     if (!fs.lstatSync(currFolder + file).isDirectory()) {
+  //       files.push({name: file, type: 'file', path: `${currFolder}${file}/`})
+  //     }
+  //   })
+  // }
