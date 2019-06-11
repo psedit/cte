e054e87a4a51038c7478c8ef6b583d0be88bb06d
Mund Vetter <mundvetter@gmail.com>
2019-06-11 17:27:17 +0200

    correct file names used

--- src/client/src/renderer/store/modules/fileTracker.js
+++ src/client/src/renderer/store/modules/fileTracker.js
@@ -1,7 +1,9 @@
+import Tab from '../../components/Tabs/tabType'
 const fs = require('fs')
+
 const state = {
   code: '',
-  openedFile: '',
+  openFile: '',
   filePaths: '',
   tabs: []
 }
@@ -10,39 +12,47 @@ const mutations = {
   updateCode (state, newCode) {
     state.code = newCode
   },
-  /** Adds a tab to state
+  /**
+   * Adds a tab to state
    * @param {Object} state vuex state
-   * @param {string} newTab the tab that need to be added
+   * @param {string} filePath the filepath that needs to be added as tab
    */
-  addTab (state, newTab) {
-    if (!state.tabs.includes(newTab)) {
+  addTab (state, filePath) {
+    if (state.tabs.every(x => x.filePath !== filePath)) {
+      const newTab = new Tab(filePath)
       state.tabs = [...state.tabs, newTab]
     }
   },
-  /** Removes a tab from state
+  /**
+   * Removes a tab from state
    * @param {Object} state vuex state
-   * @param {string} tabToRemove the tab that needs to be removed
+   * @param {Tab} tabToRemove the tab that needs to be removed
    */
   removeTab (state, tabToRemove) {
-    state.tabs.filter(x => x !== tabToRemove)
+    state.tabs.filter(x => x.filePath !== tabToRemove.filePath)
   },
-  /** Updates the filepaths
+  /**
+   * Updates the filepaths
    * @param {Object} state
    * @param {Object[]} filePaths
    */
   updateFiles (state, filePaths) {
     state.filePaths = filePaths
+  },
+  updateOpenFile (state, filePath) {
+    state.openFile = filePath
   }
 }
 
 const actions = {
-  /** Opens a file from the root of the project, and updates the code.
+  /**
+   * Opens a file from the root of the project, and updates the code.
    * Also changes the openedFile state. Add the file to the tabs.
    * @param {Object} state vuex state
    * @param {string} filePath the file path to document to be opened
    */
   openFile (state, filePath) {
-    state.openedFile = filePath
+    state.commit('updateOpenFile', filePath)
     state.commit('addTab', filePath)
     fs.readFile(filePath.substring(0, filePath.length - 1), 'utf8', (err, data) => {
       if (err) {
