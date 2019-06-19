3bab190a55aea08e6dd71079e433ebeb63c3a2b4
Sam van Kampen <sam@tehsvk.net>
2019-06-19 04:30:29 +0200

    g and q do look alike. so much so that I didn't notice this bug for a WHILE.

--- src/client/src/main/connector.js
+++ src/client/src/main/connector.js
@@ -2,9 +2,7 @@ const WebSocket = require('ws')
 const uuid = require('uuid/v4')
 
 // FIXME: Change path to server path.
-// const path = new URL('ws://bami.party:12345')
-const path = new URL('ws://seqfault.party:12345')
-// const path = new URL('ws://localhost:8080')
+const path = new URL('ws://segfault.party:12345')
 
 /**
  * @typedef {Object} Message
