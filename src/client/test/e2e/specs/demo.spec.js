5174eb5d17c9db62bdf17678bd61b2636b34af7c
Mund Vetter <mundvetter@gmail.com>
2019-06-25 13:24:00 +0200

    added open file e2e

--- src/client/test/e2e/specs/demo.spec.js
+++ src/client/test/e2e/specs/demo.spec.js
@@ -1,7 +1,6 @@
 /* globals expect describe it $ */
 import utils from '../utils'
 const uuid = require('uuid/v4')
-
 const name = uuid()
 
 describe("Demo", function () {
@@ -14,8 +13,18 @@ describe("Demo", function () {
         expect(title).to.equal('cte-client')
       })
   })
+
   it('should create a file', async function () {
     const text = await this.app.client.element('.material-design-icon.file-plus-icon.button').click().keys([name]).element('button.ok').click().element('ul.fileTree').getText()
-    expect(text.split('\n')).to.include(name)
+    return expect(text.split('\n')).to.include(name)
+  })
+
+  it('should open a file', async function () {
+    const tabFile = 'export default Tab'
+    await this.app.client.waitUntilWindowLoaded()
+    await this.app.client.element('span=tabType.js').click().waitUntilTextExists('#tabs', 'tabType.js', 3000)
+    const text = await this.app.client.element('.editor-pieces').getText()
+    const arr = text.split('\n')
+    return expect(arr[arr.length - 1]).to.equal(tabFile)
   })
 })
