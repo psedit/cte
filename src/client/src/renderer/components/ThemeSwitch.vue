6a0d34a20d4af208665bbc74821e35a9958299e7
Jim Wagemans <jimwagemans@gmail.com>
2019-06-26 10:02:05 +0200

    Vergeten bestand toegevoegd

---
+++ src/client/src/renderer/components/ThemeSwitch.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <lightbulb-outline v-if="lightTheme" id="theme-button-on" class="button" @click="turnOff"/>
+    <lightbulb v-if="!lightTheme" id="theme-button-off" class="button" @click="turnOn"/>
+  </div>
+</template>
+
+<script>
+import LightbulbOutline from 'vue-material-design-icons/LightbulbOutline'
+import Lightbulb from 'vue-material-design-icons/Lightbulb'
+export default {
+  name: 'theme-button',
+  components: {
+    LightbulbOutline,
+    Lightbulb
+  },
+  data () {
+    return {
+      lightTheme: false
+    }
+  },
+  methods: {
+    turnOn () {
+      this.lightTheme = true
+      this.$emit('theme-change', this.lightTheme)
+    },
+    turnOff () {
+      this.lightTheme = false
+      this.$emit('theme-change', this.lightTheme)
+    }
+  }
+}
+</script>
