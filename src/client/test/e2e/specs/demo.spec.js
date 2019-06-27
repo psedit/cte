cff809e904bbb5f13a48460b63da0b84c0c41b39
Jorik Smorenburg <jorik.smorenburg@gmail.com>
2019-06-27 12:26:32 +0200

    Robot tester now stores it's files in a separate folder named TEST

--- src/client/test/e2e/specs/demo.spec.js
+++ src/client/test/e2e/specs/demo.spec.js
@@ -14,13 +14,18 @@ describe('Demo', function () {
       })
   })
 
-  it('should create a file', function () {
+  it('should create a file in the folder TEST', async function () {
+    await this.app.client.waitUntilWindowLoaded()
+    await this.app.client.waitUntilTextExists('span=TEST', 'TEST', 10000)
+    await this.app.client.element('span=TEST').click()
     return this.app.client.element('.material-design-icon.file-plus-icon.button').click().keys([name]).element('button.ok').click().element('ul.fileTree').getText().waitUntilTextExists(`span=${name}`, name, 3000)
   })
 
   it('should open a file', async function () {
     const tabFile = 'export default Tab'
     await this.app.client.waitUntilWindowLoaded()
+    await this.app.client.waitUntilTextExists('span=TEST', 'TEST', 10000)
+    await this.app.client.element('span=TEST').click()
     await this.app.client.waitUntilTextExists('span=test.js', 'test.js', 10000)
     await this.app.client.element('span=test.js').click().waitForVisible('.CodeMirror-lines', 10000)
     const text = await this.app.client.element('.editor-pieces').getText()
